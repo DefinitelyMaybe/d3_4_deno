b5685dc3afc5c3be2b2f8d270b255b275f4d72cb
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-10-23 11:51:07 +1300

    deno fmt

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import {
-  ensureDirSync,
+  emptyDirSync,
   ensureFileSync,
   existsSync,
   walkSync,
@@ -37,14 +37,14 @@ const d3modules = [
   "d3-transition",
   "d3-zoom",
 ];
-const d3Dir = "d3/";
+const d3Dir = "src/";
 
 function fetchTypes(path: string, url: string) {
   fetch(url).then((res) => {
     if (res.status == 200) {
       res.text().then((text) => {
         ensureFileSync(path);
-        text = text.replace(/^/, '/// <reference lib="dom" />\n')
+        text = text.replace(/^/, '/// <reference lib="dom" />\n');
         Deno.writeTextFileSync(path, text);
       });
     } else {
@@ -56,7 +56,7 @@ function fetchTypes(path: string, url: string) {
 function identifyMissingFiles() {
   // walk through the dir looking for imports of .js files which don't exist in the dir
   const missedFiles: string[] = [];
-  for (const entry of walkSync("d3")) {
+  for (const entry of walkSync(d3Dir)) {
     if (entry.isFile && entry.name.endsWith(".js")) {
       const src = Deno.readTextFileSync(entry.path);
       // match against .js imports
@@ -67,10 +67,12 @@ function identifyMissingFiles() {
           // construct the path using the match
           const x = match[0].split("../");
           const matchPath = x[x.length - 1];
-          const splitPath = entry.path.split("\\")
-          
-          let relativePath = `${d3Dir}${splitPath[splitPath.length-3]}/${matchPath}`;
-          
+          const splitPath = entry.path.split("\\");
+
+          let relativePath = `${d3Dir}${
+            splitPath[splitPath.length - 3]
+          }/${matchPath}`;
+
           // if file doesn't already exist, add it to the list
           if (!existsSync(relativePath)) {
             // clean up the path before adding it to the list
@@ -81,17 +83,16 @@ function identifyMissingFiles() {
               // console.log(`found import from: ${entry.path}\t\t\t${relativePath}`);
               // ignore mod.js urls on re-runs
               if (!relativePath.includes("mod.js")) {
-                missedFiles.push(relativePath); 
+                missedFiles.push(relativePath);
               }
             }
           }
-
         } else if (match[0].includes("./")) {
           // construct the path using the match
           const x = match[0].split("./");
           const matchPath = x[x.length - 1];
           let relativePath = entry.path.split(entry.name)[0] + matchPath;
-  
+
           // console.log(`${entry.path}, ${match[0]}`);
 
           // if file doesn't already exist, add it to the list
@@ -132,57 +133,68 @@ function identifyMissingFiles() {
   return missedFiles;
 }
 
-function fetchFile(path:string) {
+function fetchFile(path: string) {
   // create the url
-  const moduleName = path.split("/")[0];
+  const moduleName = path.split("/")[1];
   const relativePath = path.split(moduleName)[1];
   const srcURL =
     `https://raw.githubusercontent.com/d3/${moduleName}/master/src${relativePath}`;
 
   return fetch(srcURL)
-  .then((res)=> {
-    if (res.status == 200) {
-      return res.text();
-    } else {
-      console.log(`Got a ${res.status} from: ${srcURL}`);
-    } 
-  })
-  .then((text)=> {
-    if (text) {
-      // construct the path to write to.
-      const splitPath = srcURL.split("src/");
-      const relativePathJS = splitPath[splitPath.length - 1];
+    .then((res) => {
+      if (res.status == 200) {
+        return res.text();
+      } else {
+        console.log(`Got a ${res.status} from: ${srcURL}`);
+        console.log(path, moduleName, relativePath);
+      }
+    })
+    .then((text) => {
+      if (text) {
+        // construct the path to write to.
+        const splitPath = srcURL.split("src/");
+        const relativePathJS = splitPath[splitPath.length - 1];
 
-      const path = `${d3Dir}${moduleName}/`;
+        const path = `${d3Dir}${moduleName}/`;
 
-      ensureFileSync(path + relativePathJS);
+        ensureFileSync(path + relativePathJS);
 
-      // add deno ref to dom
-      text = text.replace(/^/, `/// <reference lib="dom" />\n`);
+        // add deno ref to dom
+        text = text.replace(/^/, `/// <reference lib="dom" />\n`);
 
-      Deno.writeTextFileSync(path + relativePathJS, text); 
-      return true
-    } else {
-      return false
-    }
-  })
+        Deno.writeTextFileSync(path + relativePathJS, text);
+        return true;
+      } else {
+        return false;
+      }
+    });
 }
 
-function adjustscriptURLS(path:string) {
-  let src = Deno.readTextFileSync(path)
-  
+function adjustscriptURLS(path: string) {
+  let src = Deno.readTextFileSync(path);
+
   const scriptDepth = path.split("/").length;
 
   src = src.replaceAll(/import .+? from ("|')d3-.+?("|')/g, (m) => {
     if (!m.includes("mod.js")) {
       // split via different double or single quotes
       if (m.includes("'")) {
-        let newValue = m.replace(/'d3-/g, `'${"../".repeat(scriptDepth-2)}d3-`);
-        newValue = path.endsWith(".js") ?  newValue.replace(/'$/g, "/mod.js'") : newValue.replace(/'$/g, "/mod.d.ts'");
+        let newValue = m.replace(
+          /'d3-/g,
+          `'${"../".repeat(scriptDepth - 2)}d3-`,
+        );
+        newValue = path.endsWith(".js")
+          ? newValue.replace(/'$/g, "/mod.js'")
+          : newValue.replace(/'$/g, "/mod.d.ts'");
         return newValue;
       } else {
-        let newValue = m.replace(/"d3-/g, `"${"../".repeat(scriptDepth-2)}d3-`);
-        newValue = path.endsWith(".js") ? newValue.replace(/"$/g, '/mod.js"') : newValue.replace(/"$/g, '/mod.d.ts"');
+        let newValue = m.replace(
+          /"d3-/g,
+          `"${"../".repeat(scriptDepth - 2)}d3-`,
+        );
+        newValue = path.endsWith(".js")
+          ? newValue.replace(/"$/g, '/mod.js"')
+          : newValue.replace(/"$/g, '/mod.d.ts"');
         return newValue;
       }
     } else {
@@ -190,12 +202,12 @@ function adjustscriptURLS(path:string) {
     }
   });
   // single edge case for this multiline import
-  src = src.replaceAll(/import {.+? from ("|')d3-.+?("|')/gs, (m)=> {
+  src = src.replaceAll(/import {.+? from ("|')d3-.+?("|')/gs, (m) => {
     // 4 needs to have depth of 2 i.e. ../../
-    m = m.replace(/d3-/g, `../d3-`)
-    m = m.replace(/"$/g, '/mod.js"')
-    return m
-  })
+    m = m.replace(/d3-/g, `../d3-`);
+    m = m.replace(/"$/g, '/mod.js"');
+    return m;
+  });
   // write the new urls to write
   Deno.writeTextFileSync(path, src);
 }
@@ -232,7 +244,7 @@ function initIndex(moduleName: string) {
         // add deno ref to types and dom
         text = text.replace(
           /^/,
-          `/// <reference lib="dom" />\n// @deno-types="./${relativePathTS}"\n`,
+          `/// <reference lib="dom" />\n/// <reference types="./${relativePathTS}" />\n`,
         );
 
         Deno.writeTextFileSync(path + relativePathJS, text);
@@ -245,89 +257,97 @@ function initIndex(moduleName: string) {
     });
 }
 
-ensureDirSync(d3Dir);
+// start with a clean slate each time
+emptyDirSync(d3Dir);
 
-if (!existsSync(`${d3Dir}mod.js`)) {
-  for (let index = 0; index < d3modules.length; index++) {
-    const moduleName = d3modules[index];
-    await initIndex(moduleName);
-  }
-  // create the mod.js and mod.d.ts files
-  let modSRC = '/// <reference lib="dom" />\n';
-  d3modules.forEach((name) => {
-    modSRC += `// @deno-types="./${name}/mod.d.ts"\nexport * from "./${name}/mod.js"\n`;
-  });
-  Deno.writeTextFileSync(`${d3Dir}mod.js`, modSRC);
+// gather initial index.js files and rename them to mod.js files
+const initIndices = [];
+for (let index = 0; index < d3modules.length; index++) {
+  const moduleName = d3modules[index];
+  initIndices.push(initIndex(moduleName));
 }
+await Promise.all(initIndices);
 
-let c = 1
+// create the mod.js and mod.d.ts files
+let modSRC = '/// <reference lib="dom" />\n';
+d3modules.forEach((name) => {
+  modSRC +=
+    `/// <reference types="./${name}/mod.d.ts" />\nexport * from "./${name}/mod.js"\n`;
+});
+Deno.writeTextFileSync(`${d3Dir}mod.js`, modSRC);
+
+let c = 1;
 let missing = identifyMissingFiles();
 
 while (missing.length != 0) {
   console.log(`round ${c}: collecting ${missing.length} files`);
   // make a list of promises
-  const files = []
+  const files = [];
   for (let index = 0; index < missing.length; index++) {
     const path = missing[index];
-    files.push(fetchFile(path))
+    files.push(fetchFile(path));
   }
   // wait for all the promises to resolve
-  await Promise.all(files)
-  c += 1
-  missing = identifyMissingFiles()
+  await Promise.all(files);
+  c += 1;
+  missing = identifyMissingFiles();
 }
 
 // Once the loop finishes we can walk through the dir and update the d3 urls
 for (const entry of walkSync(d3Dir)) {
   if (entry.isFile) {
     // I like to replace the backslashes for working with strings
-    adjustscriptURLS(entry.path.replaceAll(/\\/g, "/"))
+    adjustscriptURLS(entry.path.replaceAll(/\\/g, "/"));
   }
 }
 
 // lastly adjust specific files
-const geoJsonURL = `https://raw.githubusercontent.com/caseycesari/GeoJSON.js/master/geojson.js`
-const geoJsonTypesURL = `https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/geojson/index.d.ts`
-const delaunayURL = `https://raw.githubusercontent.com/mapbox/delaunator/master/index.js`
-const delaunayTypesURL = `https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/d3-delaunay/index.d.ts`
-const d3geoFile = "d3/d3-geo/mod.d.ts"
-const d3contourFile = "d3/d3-contour/mod.d.ts"
-const delaunayFile = "d3/d3-delaunay/delaunay.js"
+const geoJsonURL =
+  `https://raw.githubusercontent.com/caseycesari/GeoJSON.js/master/geojson.js`;
+const geoJsonTypesURL =
+  `https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/geojson/index.d.ts`;
+const delaunayURL =
+  `https://raw.githubusercontent.com/mapbox/delaunator/master/index.js`;
+const delaunayTypesURL =
+  `https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/d3-delaunay/index.d.ts`;
+const d3geoFile = `${d3Dir}d3-geo/mod.d.ts`;
+const d3contourFile = `${d3Dir}d3-contour/mod.d.ts`;
+const delaunayFile = `${d3Dir}d3-delaunay/delaunay.js`;
 
-let src = Deno.readTextFileSync(d3geoFile)
-src = src.replace(/import \* as GeoJSON from 'geojson';/g, (m)=> {
-  m = m.replace(/'geojson'/g, `'${geoJsonURL}'`)
-  m = m.replace(/^/g, `// @deno-types="${geoJsonTypesURL}"\n`)
-  return m
-})
-Deno.writeTextFileSync(d3geoFile, src)
+let src = Deno.readTextFileSync(d3geoFile);
+src = src.replace(/import \* as GeoJSON from 'geojson';/g, (m) => {
+  m = m.replace(/'geojson'/g, `'${geoJsonURL}'`);
+  m = m.replace(/^/g, `/// <reference types="${geoJsonTypesURL}" />\n`);
+  return m;
+});
+Deno.writeTextFileSync(d3geoFile, src);
 
-src = Deno.readTextFileSync(d3contourFile)
-src = src.replace(/import { MultiPolygon } from 'geojson';/g, (m)=> {
-  m = m.replace(/'geojson'/g, `'${geoJsonURL}'`)
-  m = m.replace(/^/g, `// @deno-types="${geoJsonTypesURL}"\n`)
-  return m
-})
-Deno.writeTextFileSync(d3contourFile, src)
+src = Deno.readTextFileSync(d3contourFile);
+src = src.replace(/import { MultiPolygon } from 'geojson';/g, (m) => {
+  m = m.replace(/'geojson'/g, `'${geoJsonURL}'`);
+  m = m.replace(/^/g, `/// <reference types="${geoJsonTypesURL}" />\n`);
+  return m;
+});
+Deno.writeTextFileSync(d3contourFile, src);
 
 // create the delaunator file and reference it from the delaunay file
 // this is because raw.github... responds with MIME type of "text/plain".
 // if someone wanted to use this part of d3 within their script they
 // couldn't use delaunay and would run into troubles trying to import
 // directly from src/mod.js
-const res = await fetch(delaunayURL)
-const resSRC = await res.text()
-Deno.writeTextFileSync("d3/d3-delaunay/delaunator.js", resSRC)
-src = Deno.readTextFileSync(delaunayFile)
-src = src.replace(/import Delaunator from "delaunator";/g, (m)=> {
-  m = m.replace(/"delaunator"/g, `"./delaunator.js"`)
-  m = m.replace(/^/g, `// @deno-types="${delaunayTypesURL}"\n`)
-  return m
-})
-Deno.writeTextFileSync(delaunayFile, src)
+const res = await fetch(delaunayURL);
+const resSRC = await res.text();
+Deno.writeTextFileSync(`${d3Dir}d3-delaunay/delaunator.js`, resSRC);
+src = Deno.readTextFileSync(delaunayFile);
+src = src.replace(/import Delaunator from "delaunator";/g, (m) => {
+  m = m.replace(/"delaunator"/g, `"./delaunator.js"`);
+  m = m.replace(/^/g, `/// <reference types="${delaunayTypesURL}" />\n`);
+  return m;
+});
+Deno.writeTextFileSync(delaunayFile, src);
 
 const p = Deno.run({
-  cmd: ["deno", "fmt", "d3"],
+  cmd: ["deno", "fmt", d3Dir],
 });
 
 await p.status();
